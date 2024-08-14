434837390c69902b6c9c43df44a4e54c1e8a5d5f
Nicolás Poza <npoza.vrgroup@bicevida.cl>
2024-08-14 13:48:21 -0400

    first commit

---
+++ src/test/java/com/botproyect/bot/BotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.botproyect.bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
